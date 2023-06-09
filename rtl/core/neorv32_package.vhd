bcb94e0c15a6c43c072bd04734b47b53b249b59a
bob frankel <bob@biosbob.biz>
2023-06-09 11:37:38 -0700

    ILEN = 16

--- rtl/core/neorv32_package.vhd
+++ rtl/core/neorv32_package.vhd
@@ -63,6 +63,8 @@ package neorv32_package is
     constant hw_version_c : std_ulogic_vector(31 downto 0) := x"01080502"; -- hardware version
     constant archid_c : natural := 19; -- official RISC-V architecture ID
     constant XLEN : natural := 32; -- native data path width, do not change!
+    constant ILEN : natural := 16; -- native inst addr width, do not change!
+
 
     -- Check if we're inside the Matrix -------------------------------------------------------
     -- -------------------------------------------------------------------------------------------
